5ee8971ac5134afbe78f0e844e80ed97a2f1d9c5
dumptyd <dumpty.driller@gmail.com>
2016-07-13 17:58:02 +0530

    added everything in a class

--- index.js
+++ index.js
@@ -4,50 +4,56 @@ var eventEmitter = new events.EventEmitter();
 var url = 'http://front1.omegle.com/start';
 var useragent = 'Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)';
 
-function getResponse(path,data)
-{
-	var options = {
-	  url: url+path,
-	  headers: {
-		'User-Agent': useragent,
-		'Connection': 'Keep-Alive'
-	  },
-	  method: 'POST',
-	  qs: data
-	};
+
+var Omegle = {
 	
-	request(options, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-		return body;
-	  }
-	  else
-	  {
-		console.log('Error: '+ path);
-		return false;
-	  }
-	})
-}
-
-function connect()
-{
-	var data = {
-		rcs: 1,
-        firstevents: 1,
-        lang: 'en'
+	this.id='';
+	
+	this.getResponse= function(path,data){
+		var options = {
+		  url: url+path,
+		  headers: {
+			'User-Agent': useragent,
+			'Connection': 'Keep-Alive'
+		  },
+		  method: 'POST',
+		  qs: data
 		};
-	var body = getResponse('/start',data);
-	if(!body)
-		eventEmitter.emit('connecting',JSON.parse(body).clientID);
+		
+		request(options, function (error, response, body) {
+		  if (!error && response.statusCode == 200) {
+			return body;
+		  }
+		  else
+		  {
+			console.log('Error: '+ path);
+			return false;
+		  }
+		})
+	}
+
+	this.connect=function(){
+		var data = {
+			rcs: 1,
+			firstevents: 1,
+			lang: 'en'
+			};
+		var body = getResponse('/start',data);
+		if(!body)
+		{
+			this.id=JSON.parse(body).clientID;
+			eventEmitter.emit('connecting');
+		}
+	}
+
+	this.getEvents=function(){
+		var data={id:}
+	}
 }
 
-function getEvents()
-{
-	var data={id:}
-}
-
-connect();
-
-eventEmitter.on('connecting',function(id){
-	console.log('Connected as: ' + id);
+var om = new Omegle();
+om.connect();
+eventEmitter.on('connecting',function(){
+	console.log('Connected as: ' + om.id);
 });
 
